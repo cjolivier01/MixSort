9230e5de3e1daaf014ffc6c5ea67c137a3e5dda7
Chris Olivier <colivier@tesla.com>
2023-10-13 08:32:02 -0700

    ..

--- train.sh
+++ train.sh
@@ -1,1 +1,3 @@
-./p tools/train.py -f exps/example/mot/yolox_x_ch.py -d 8 -b 48 --fp16 -o
+#./p tools/train.py -f exps/example/mot/yolox_x_ch.py -d 8 -b 48 --fp16 -o
+#./p tools/train.py -f exps/example/mot/yolox_x_ch.py -d 8 -b 24 --fp16
+./p tools/train.py -f exps/example/mot/yolox_x_ch.py -d 8 -b 32 --fp16  -c pretrained/yolox_x.pth
