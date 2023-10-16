d9adf33a4fc1579a48b6f9f67406f639675ce0df
Chris Olivier <colivier@tesla.com>
2023-10-16 08:51:04 -0700

    ..

--- slurm.sh
+++ slurm.sh
@@ -5,6 +5,6 @@
 
 #  srun --tasks-per-node 8 --nodelist $RUN_PARTITION_SERVERS -p sw-mpu --cpus-per-task 12 \
 
-srun --tasks-per-node 8 -N 4 -p sw-mpu --cpus-per-task 12 \
+srun --tasks-per-node 1 -N 4 -p sw-mpu --exclusive \
     ./p tools/train.py -f exps/example/mot/yolox_x_hockey.py -d 8 -b 64 --fp16 -c pretrained/yolox_x_sports_train.pth
 
