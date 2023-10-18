d8e27bb32e3fcd8b7a4d42f9b02a3805fcbb7cb9
Chris Olivier <colivier@tesla.com>
2023-10-18 08:14:09 -0700

    ..

--- train-1.sh
+++ train-1.sh
@@ -4,7 +4,7 @@ OMP_NUM_THREADS=16 \
   MASTER_ADDRESS="192.168.221.61" \
   ./p tools/train.py \
     --local_rank=0 \
-    --start_epoch=3 \
+    --start_epoch=4 \
     --num_machines=4 \
     --machine_rank=0 \
     -f exps/example/mot/yolox_x_hockey.py -d 8 -b 64 --fp16 -c YOLOX_outputs/yolox_x_hockey/latest_ckpt.pth.tar
