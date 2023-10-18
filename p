a27abf373e2a52332ab94bf0ae3ba92dd53fe214
Chris Olivier <colivier@tesla.com>
2023-10-18 08:42:57 -0700

    ..

--- p
+++ p
@@ -1,4 +1,5 @@
 #!/bin/bash
 LD_LIBRARY_PATH="${HOME}/cuda/lib64:$LD_LIBRARY_PATH" \
+  OMP_NUM_THREADS=16 \
   PYTHONPATH=$(pwd) \
   python3 $@
--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-#RUN_PARTITION_SERVERS="mojo017,mojo-26u-r06u[03,05,07,09]"
 
-#srun --tasks-per-node 8 -N 4 -p sw-mpu --cpus-per-task 12 \
+GPUS_PER_HOST=4
+BATCH_SIZE_PER_GPU=1
+HOST_COUNT=2
 
-#  srun --tasks-per-node 8 --nodelist $RUN_PARTITION_SERVERS -p sw-mpu --cpus-per-task 12 \
-
-#--nodelist mojo-ep2-r02u47 \
+TOTAL_BATCH_SIZE=$(( $GPUS_PER_HOST * $BATCH_SIZE_PER_GPU * $HOST_COUNT ))
+echo "TOTAL_BATCH_SIZE=$TOTAL_BATCH_SIZE"
 
 srun --tasks-per-node 1 \
-  -N 2 \
-  --cpus-per-task=90 \
-  -p sw-mpu  \
-  --exclusive \
-    ./p tools/train.py -f exps/example/mot/yolox_x_hockey.py -d 8 -b 32 --fp16 -c ./YOLOX_outputs/yolox_x_hockey/latest_ckpt.pth.tar
+    -N ${HOST_COUNT} \
+    --cpus-per-task=90 \
+    -p sw-gpu  \
+    --exclusive \
+      ./p tools/train.py \
+      -f exps/example/mot/yolox_x_hockey.py \
+      -d ${GPUS_PER_HOST} \
+      -b ${TOTAL_BATCH_SIZE} \
+      --fp16 \
+      -c ./YOLOX_outputs/yolox_x_hockey/latest_ckpt.pth.tar
