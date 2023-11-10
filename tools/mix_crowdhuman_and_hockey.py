bf9a82a52a72bc62a35516d0a82b248d59cec4ea
Christopher Olivier <colivier@tesla.com>
2023-11-10 14:10:42 -0800

    ...

--- tools/mix_crowdhuman_and_hockey.py
+++ tools/mix_crowdhuman_and_hockey.py
@@ -12,18 +12,25 @@ def process(split="train"):
 
     all_image_ids = set()
     all_annotation_ids = set()
+    all_video_ids = set()
+    video_id_mapping = dict()
 
     max_img = 100000
     max_ann = 2000000
-    max_video = 100
+    max_video = 1000
 
     base_dir = os.path.join(os.getcwd(), 'datasets')
 
-    train_json = json.load(open(f'{base_dir}/crowdhuman/annotations/{split}.json','r'))
+    split_json = json.load(open(f'{base_dir}/crowdhuman/annotations/{split}.json','r'))
 
     img_list = list()
+    ann_list = list()
+    
+    #
+    # CH (image dataset)
+    #
     img_id_count = 0
-    for img in train_json['images']:
+    for img in split_json['images']:
         img_id_count += 1
         img['file_name'] = f'{base_dir}/crowdhuman/Crowdhuman_{split}/' + img['file_name']
         img['frame_id'] = img_id_count
@@ -37,8 +44,9 @@ def process(split="train"):
         img['video_id'] = max_video
         img_list.append(img)
 
-    ann_list = list()
-    for ann in train_json['annotations']:
+    anno_count = 0
+    for ann in split_json['annotations']:
+        anno_count += 1
         annotation_id = ann['id'] + max_ann
         assert annotation_id not in all_annotation_ids
         all_annotation_ids.add(annotation_id)
@@ -47,14 +55,59 @@ def process(split="train"):
         assert ann['category_id'] == 1
         ann_list.append(ann)
 
+
     video_list.append({
         'id': max_video,
         'file_name': f'crowdhuman_{split}'
     })
+    video_id_mapping[max_video] = max_video
 
+    #
+    # HDS (video dataset)
+    #
+    split_json = json.load(open(f'{base_dir}/hockeyTrackingDataset/annotations/{split}.json','r'))
+
+    max_img += int(img_id_count * 2)
+    max_ann += int(anno_count * 2)
     max_video += 1
 
-    print(f'crowdhuman_{split}')
+    img_id_count = 0
+    anno_count = 0
+    
+    for vid in split_json['videos']:
+        old_video_id = vid['id']
+        new_video_id = vid['id'] + max_video 
+        assert new_video_id not in all_video_ids
+        assert old_video_id not in video_id_mapping
+        all_video_ids.add(new_video_id)
+        video_id_mapping[old_video_id] = new_video_id
+    
+    for img in split_json['images']:
+        img['file_name'] = f'{base_dir}/hockeyTrackingDataset/{split}/' + img['file_name']
+        # No prev for non-sequence-video
+        image_id = img['id'] + max_img
+        assert image_id not in all_image_ids
+        all_image_ids.add(image_id)
+        img['id'] = image_id
+        if img['prev_image_id'] >= 0:
+            img['prev_image_id'] = img['prev_image_id'] + max_img
+        img['next_image_id'] = img['next_image_id'] + max_img
+        img['video_id'] = video_id_mapping[img['video_id']]
+        img_list.append(img)
+    
+    for ann in split_json['annotations']:
+        annotation_id = ann['id'] + max_ann
+        assert annotation_id not in all_annotation_ids
+        all_annotation_ids.add(annotation_id)
+        ann['id'] = annotation_id
+        ann['image_id'] = ann['image_id'] + max_img
+        assert ann['category_id'] == 1
+        # Set new catagory as 'player'
+        ann['category_id'] = 3
+        ann_list.append(ann)
+    
+
+    print(f'hockeyTrackingDataset-{split}')
 
     mix_json = dict()
     mix_json['images'] = img_list
